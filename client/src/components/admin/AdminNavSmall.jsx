1329c4b835e06bdaa386fd18f2f53b81233df56c
Uzair Alam <a-8278@kmclu.ac.in>
2024-11-08 16:27:55 +0530

    fix: admin mobile nav issue

--- client/src/components/admin/AdminNavSmall.jsx
+++ client/src/components/admin/AdminNavSmall.jsx
@@ -22,78 +22,87 @@ const AdminNavSmall = () => {
           : "bg-white text-black"
       } flex flex-col justify-between items-start h-[90dvh]`}
     >
-
-        <div className="nav-links flex-col flex-1 items-start w-full">
-          <NavLink
-            href="/admin/users"
-            label="Users"
-            style={(theme) => ({
-              root: {
-                "&:hover": {
-                  backgroundColor: theme.colors.teal[1],
-                  color: theme.colors.teal[9],
-                },
+      <div className="nav-links flex-col flex-1 items-start w-full">
+        <NavLink
+          component={Link}
+          to="/admin/users"
+          label="Users"
+          style={(theme) => ({
+            root: {
+              "&:hover": {
+                backgroundColor: theme.colors.teal[1],
+                color: theme.colors.teal[9],
               },
-            })}
-            active={
-              location.pathname == "/admin" ||
-              location.pathname.includes("/admin/users")
-            }
-            leftSection={<CiUser className="text-2xl" />}
-          />
-          <NavLink
-            href="/admin/companies"
-            label="Listings"
-            leftSection={<CiViewList className="text-2xl" />}
-            active={location.pathname.includes("/admin/companies")}
-            rightSection={<FaChevronRight />}
+            },
+          })}
+          active={
+            location.pathname == "/admin" ||
+            location.pathname.includes("/admin/users")
+          }
+          leftSection={<CiUser className="text-2xl" />}
+        />
+        <NavLink
+          component={Link}
+          to="/admin/companies"
+          label="Listings"
+          leftSection={<CiViewList className="text-2xl" />}
+          active={location.pathname.includes("/admin/companies")}
+          rightSection={<FaChevronRight />}
+        >
+          <ScrollArea
+            offsetScrollbars
+            scrollbarSize={6}
+            scrollHideDelay={500}
+            h={400}
+            className="w-full"
           >
-            <ScrollArea
-        offsetScrollbars
-        scrollbarSize={6}
-        scrollHideDelay={500} h={400} className="w-full">
-              <NavLink
-                href={`/admin/companies/all`}
-                active={location.pathname.includes("all")}
-                label="All"
-                childrenOffset={14}
-              />
-              {Object.values(categories).map((category) => {
-                return (
-                  <NavLink
-                    key={category?.name}
-                    href={`/admin/companies/${category?.name}`}
-                    active={location.pathname.includes(category?.name)}
-                    label={
-                      category?.name.charAt(0).toUpperCase() +
-                      category?.name.slice(1)
-                    }
-                    childrenOffset={14}
-                  />
-                );
-              })}
-            </ScrollArea>
-          </NavLink>
+            <NavLink
+              component={Link}
+              to={`/admin/companies/all`}
+              active={location.pathname.includes("all")}
+              label="All"
+              childrenOffset={14}
+            />
+            {Object.values(categories).map((category) => {
+              return (
+                <NavLink
+                  key={category?.name}
+                  component={Link}
+                  to={`/admin/companies/${category?.name}`}
+                  active={location.pathname.includes(category?.name)}
+                  label={
+                    category?.name.charAt(0).toUpperCase() +
+                    category?.name.slice(1)
+                  }
+                  childrenOffset={14}
+                />
+              );
+            })}
+          </ScrollArea>
+        </NavLink>
 
-          <NavLink
-            href="/admin/reviews"
-            label="Reviews"
-            leftSection={<CiReceipt className="text-2xl" />}
-            active={location.pathname.includes("/admin/reviews")}
-          />
-          <NavLink
-            href="/admin/requests"
-            label="Pending Requests"
-            leftSection={<MdPendingActions className="text-2xl" />}
-            active={location.pathname.includes("/admin/requests")}
-          />
-          <NavLink
-            href="/admin/categories"
-            label="Categories"
-            leftSection={<MdCategory className="text-2xl" />}
-            active={location.pathname.includes("/admin/categories")}
-          />
-        </div>
+        <NavLink
+          component={Link}
+          to="/admin/reviews"
+          label="Reviews"
+          leftSection={<CiReceipt className="text-2xl" />}
+          active={location.pathname.includes("/admin/reviews")}
+        />
+        <NavLink
+          component={Link}
+          to="/admin/requests"
+          label="Pending Requests"
+          leftSection={<MdPendingActions className="text-2xl" />}
+          active={location.pathname.includes("/admin/requests")}
+        />
+        <NavLink
+          component={Link}
+          to="/admin/categories"
+          label="Categories"
+          leftSection={<MdCategory className="text-2xl" />}
+          active={location.pathname.includes("/admin/categories")}
+        />
+      </div>
       {/* </ScrollArea> */}
 
       <div className="nav-bottom">
