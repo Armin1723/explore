c202a4aaf502c974e340ed23b3428229ecc6b5ec
Uzair Alam <a-8278@kmclu.ac.in>
2024-12-05 15:14:09 +0530

    feat: enhance ContactCard layout and update button styling for improved UX

--- client/src/components/home/ContactCard.jsx
+++ client/src/components/home/ContactCard.jsx
@@ -16,23 +16,27 @@ const ContactCard = () => {
 
   return (
     <div className="flex page w-full items-center justify-center ">
-      <div className="container flex items-center p-20 max-lg:px-8 max-lg:py-6 max-sm:py-2 w-[80%] max-sm:w-[90%] rounded-lg bg-[#d9d9d9] border border-black/80 translate-y-28 max-sm:translate-y-0 max-sm:mt-8 z-[10]">
-        <div className="text w-1/2 max-lg:w-2/3 h-full flex flex-col max-sm:items-start ">
-          <div className="heading max-sm:text-sm">
+      <div className="container flex max-lg:flex-col-reverse items-center p-20 max-lg:px-8 max-lg:py-6 max-sm:py-4 w-[80%] max-sm:w-[90%] rounded-lg bg-[#d9d9d9] border border-black/80 translate-y-28 max-sm:translate-y-0 max-sm:mt-8 z-[10]">
+        <div className="text w-1/2 max-lg:w-full h-full flex flex-col max-sm:items-start ">
+          <div className="heading max-lg:text-lg max-sm:text-sm">
             Grab the Opportunity Now! and start exploring from our diverse
             collection.
           </div>
           <Link
-            className="fancy !my-4 w-60 !max-sm:w-40 max-lg:scale-75 max-sm:scale-50 max-sm:-translate-x-12"
+            className="font-['poppins'] border border-black/80 px-4 flex w-fit my-4 py-1 rounded-md relative transition-all duration-300 hover:text-white hover:border-white before:absolute before:inset-0 before:!bottom-0 before:left-0 before:h-0 before:w-0 before:rounded-md before:bg-[var(--color-primary)] before:z-[-1] hover:before:w-full hover:before:h-full before:transition-all before:duration-300"
+            to="/companies/add"
+          >Join Now </Link>
+          {/* <Link
+            className="fancy !my-4 max-sm:!my-0 w-60 !max-sm:w-40 max-sm:scale-75"
             to="/companies/add"
           >
             <span className="top-key"></span>
             <span className="text">Join Now</span>
             <span className="bottom-key-1"></span>
             <span className="bottom-key-2"></span>
-          </Link>
+          </Link> */}
         </div>
-        <div className="graphics relative w-1/2 max-lg:min-w-1/3 h-full flex items-center justify-center">
+        <div className="graphics relative w-1/2 max-lg:w-full h-full flex items-center justify-center">
           {/* <img
                 src='assets/contact.svg'
                 alt="contact"
